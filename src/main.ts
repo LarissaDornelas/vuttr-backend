a954b787dba154c92ac6ff0ed831093eed480e75
Larissa Dornelas <larissa.e.dornelas@gmail.com>
2021-05-21 08:34:31 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('VUTTR Docs')
